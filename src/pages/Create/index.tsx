6b8440726945f7b5edcc9b8a46b24dacc1b89473
LeandroSannt <lsn_slim@yahoo.com.br>
2021-11-02 21:31:33 -0300

    novo componenete

--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -1,24 +1,13 @@
 import React, { useCallback, useRef, useState } from "react";
 import { Container } from "./styles";
 import Header from "../../components/header";
-
-import Tabs from "../../components/Tabs";
+import MacroEntrega from "../../components/Macroentregas";
 
 const Create: React.FC = () => {
   return (
     <Container>
       <Header />
-      <Tabs
-        subTabs={false}
-        main={["primeiro", "segundo", "terceiro", "quarto"]}
-        //subMain={["primeiro sub", "segundo sub", "terceiro sub", "quarto sub"]}
-        component={[
-          <div>primeiro componente</div>,
-          <div>segundo componente</div>,
-          <div>terceiro componentes</div>,
-          <div>quarto componentes</div>,
-        ]}
-      />
+      <MacroEntrega />
     </Container>
   );
 };
