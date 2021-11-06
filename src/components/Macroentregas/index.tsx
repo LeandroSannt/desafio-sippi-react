1f992c8e2547225be4086d9de2c3077d29e9d12a
LeandroSannt <lsn_slim@yahoo.com.br>
2021-11-05 22:17:59 -0300

    grafico pagina de macronentregas

--- src/components/Macroentregas/index.tsx
+++ src/components/Macroentregas/index.tsx
@@ -1,10 +1,90 @@
-import React from "react";
+import React, { FormEvent, useEffect, useRef, useState } from "react";
+import "./styles.module.scss";
+import styles from "./styles.module.scss";
 
-const Macroentrega: React.FC = () => {
+interface MacronEntregaProps {
+  deliverables: Array<string>;
+  title: string;
+  duration: number;
+}
+
+const Macroentrega: React.FC<MacronEntregaProps> = ({
+  deliverables,
+  title,
+  duration,
+}) => {
+  let [conclusion, setConclusion] = useState(localStorage.getItem("mykey"));
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  async function fetchData() {
+    const storage = localStorage.getItem("mykey");
+    setConclusion(storage);
+  }
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const value = inputEl.current?.value;
+
+    if (value) {
+      localStorage.setItem("mykey", value.toString());
+
+      fetchData();
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <h1>ola mundo</h1>
-    </>
+    <main>
+      <div>
+        <section>
+          <div>
+            <h5>MACROENTREGA 1</h5>
+            <p>{title}</p>
+          </div>
+          <div>
+            <h5>DURAÇÃO</h5>
+            <p>
+              {duration}
+              <span>meses</span>
+            </p>
+          </div>
+          <div>
+            <h5>ENTREGAVEIS</h5>
+
+            {deliverables.map((item) => {
+              return <p>{item}</p>;
+            })}
+          </div>
+        </section>
+        <aside>
+          <div>
+            <div>
+              <span>% de conclusão</span>
+              <h4>{conclusion}%</h4>
+            </div>
+            <div>grafico</div>
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <span>% de conclusão</span>
+            <div>
+              <input type="number" ref={inputEl} placeholder="00" required /> %
+            </div>
+
+            <button type="submit">Atualizar progresso macroentrega</button>
+          </form>
+        </aside>
+      </div>
+
+      <div className={styles.graphic}>
+        <h2>Historico de progresso</h2>
+        <div></div>
+      </div>
+    </main>
   );
 };
 
--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -1,13 +1,21 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, { useCallback, useRef, useState } from "react";
 import { Container } from "./styles";
 import Header from "../../components/header";
 import MacroEntrega from "../../components/Macroentregas";
 
+const arr = ["entrega 1", "entrega 2", "entrega 3"];
+
 const Create: React.FC = () => {
+  const [perc, usePerc] = useState(2);
   return (
     <Container>
       <Header />
-      <MacroEntrega />
+      <MacroEntrega
+        deliverables={arr}
+        title={"titulo teste de macro entrega"}
+        duration={3.5}
+      />
     </Container>
   );
 };
